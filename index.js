5809b270a8ba099bddb103070c724bdb1101b0f7
amiedawn <amie.d.carroll@gmail.com>
2020-11-19 11:48:47 -0700

    setting up files

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+//const inquirer = require('inquirer');
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
