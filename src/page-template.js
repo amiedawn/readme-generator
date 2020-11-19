ca4bb4f745e858a18d3a6e0015ee92403d076270
amiedawn <amie.d.carroll@gmail.com>
2020-11-19 14:58:21 -0700

    add files

--- src/page-template.js
+++ src/page-template.js
@@ -6,7 +6,7 @@ const generatePage = (name, github) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
+    <title>README-Generator Demo</title>
   </head>
 
   <body>
@@ -16,3 +16,5 @@ const generatePage = (name, github) => {
   </html>
   `;
 };
+
+module.exports = generatePage;
